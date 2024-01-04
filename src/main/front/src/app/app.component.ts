76d37de939ed32ad0c762b982a5fe6045176d26f
GregorGolen <gregorgolen@gmail.com>
2024-01-04 14:05:56 +0100

    front

--- src/main/front/src/app/app.component.ts
+++ src/main/front/src/app/app.component.ts
@@ -1,14 +1,50 @@
 import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { RouterOutlet } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+interface Pet {
+  id: number;
+  name: string;
+  age: number;
+  description: string;
+}
 
 @Component({
   selector: 'app-root',
-  standalone: true,
-  imports: [CommonModule, RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
   title = 'front';
+  pets: Pet[] = [];
+
+  constructor(private http: HttpClient) {
+    this.getAllPets().subscribe(data => this.pets = data);
+  }
+
+  getAllPets(): Observable<Pet[]> {
+    return this.http.get<Pet[]>('/api/pets');
+  }
+
+  createPet(name: string, age: number, description: string): void {
+    const formData = new FormData();
+    formData.append('name', name);
+    formData.append('age', age.toString());
+    formData.append('description', description);
+
+    this.http.post<Pet>('/api/pets', formData).subscribe(
+      data => {
+        this.pets.push(data);
+      },
+      error => {
+        console.log(error);
+      }
+    );
+  }
+
+  deletePet(id: number): void {
+    this.http.delete(`/api/pets/${id}`).subscribe(
+      () => this.pets = this.pets.filter(pet => pet.id !== id)
+    );
+  }
 }
